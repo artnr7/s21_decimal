8dcbb3297951b739f48ea2cb9e71d31be43643e3
Major Sweet <majorswe@un-k3.kzn.21-school.ru>
2025-02-01 14:28:30 +0300

    Make edit

--- src/s21_decimal.h
+++ src/s21_decimal.h
@@ -1,12 +1,12 @@
 #ifndef S21_DECIMAL_H
 #define S21_DECIMAL_H
 
-#include "./include/type.h"
 #include "./include/arithmetic.h"
 #include "./include/binary_operators.h"
-#include "./include/converter.h"
 #include "./include/compare.h"
+#include "./include/converter.h"
 #include "./include/round.h"
+#include "./include/type.h"
 #include "./include/utils.h"
 
 #endif
